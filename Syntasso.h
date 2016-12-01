3ab1cceca3dc46119d02125ec628faaa31c8aa73
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-12-01 15:53:46 -0800

    Add variables to header file

--- Syntasso.h
+++ Syntasso.h
@@ -8,8 +8,18 @@ class Syntasso{
 
     public:
 
-    private:
 
+    private:
+      int accumulator; // accumulator register
+      int counter;
+      int numberOfInstructions;
+      int opCode; // current operation being performed
+      int operand; // memory location of opCode being acted on
+      int instructionRegister; // next instruction to be split and run
+      int memory[1000];
+      char code[8];
+      char com[8];
+      bool hex;
 
 };
 #endif
