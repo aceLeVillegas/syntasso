862a9f1e9f8cf8032aef701f4d48f438ca8b823d
aceLeVillegas <sarahvillegas95@gmail.com>
2016-12-01 18:20:29 -0800

    add all the array values into the binCode array and set up numPar array

--- Syntasso.h
+++ Syntasso.h
@@ -8,17 +8,13 @@
 class Syntasso{
 
     public:
-<<<<<<< HEAD
-      
-=======
+
         Syntasso();
         bool checkSyntax(std::string word, int parameters); // Sarah has Sudo Code
         bool searchPar(std::string key);
         bool searchBin(std::string key);
 
 
-
->>>>>>> 7b4a1f01f446f49446c3b763d384c45e592b9bd7
 
     private:
       int accumulator; // accumulator register
@@ -29,8 +25,8 @@ class Syntasso{
       int instructionRegister; // next instruction to be split and run
       int memory[1000];
       int parameters;
-      pair<std::string, std::string> binCode[31];
-      pair<std::string, int> numPar[31];
+      pair<std::string, std::string> binCode[30];
+      pair<std::string, int> numPar[30];
 
 
 
